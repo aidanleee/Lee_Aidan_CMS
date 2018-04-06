e5dafcacb954c26b27ba58d4d8726ccda78dd318
Aidan Lee <Aidan@Aidans-MacBook-Pro.local>
2018-04-06 17:11:03 -0400

    Start Admin Index

---
+++ admin/admin_index.php
@@ -0,0 +1,18 @@
+<?php
+	ini_set('display_errors',1);
+	error_reporting(E_ALL);
+	require_once('phpscripts/config.php');
+	confirmed_logged_in();
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CMS Portal</title>
+</head>
+<body>
+
+</body>
+</html>
